92f643c199bc47ed1eb443945f2d33548a77798c
200820xmc <xing820917@gmail.com>
2025-11-22 09:16:04 +0800

    女婿酒量

---
+++ src/code1/demo.java
@@ -0,0 +1,17 @@
+package code1;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("请输入女婿的酒量：");
+            int wine = sc.nextInt();
+            if (wine > 2 ){
+                System.out.println("女婿牛逼！！！！！");
+            }else{
+                System.out.println("你也配娶我女儿？");
+            }
+        }
+    }
+}
